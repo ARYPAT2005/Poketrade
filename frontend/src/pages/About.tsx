ba51ea9abefbdf638ba8ff9f3fffb67f1e4d309e
Aryan Patel <arypat2005@gmail.com>
2025-03-27 17:37:45 -0400

    Adding cards to About Page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import "./About.css";
 const About = () => {
-  return (
-    <>
-      <h1>About</h1>
-      <p>This is the about page.</p>
-      <p style={{ marginTop: "1000px" }}>TEST1</p>
-    </>
-  );
-};
+ const [cardImage, setCards] = useState<any[]>([]);
 
+
+ useEffect(() => {
+   fetch("https://api.pokemontcg.io/v2/cards")
+     .then((response) => response.json())
+     .then(data => setCards(data.data))
+     .catch((error) => console.error("Error fetching card:", error));
+ }, []);
+
+
+ return (
+   <>
+   <div className ="Visible">
+     <h1 className="About">About PokéTrade</h1>
+     <p className="Description">PokéTrade is a web application where users can collect, trade, and manage Pokémon in a fun and interactive way. The idea is to create a simple experience, similar to collectible card games, but with a focus on trading instead of complex battles.</p>
+   </div>
+   <div className = "card-Container">
+     {cardImage.map((card) => (
+     <img src={card.images.small} alt={card.name} width="300" />
+     ))}
+   </div>
+   </>
+ );
+}
 export default About;
