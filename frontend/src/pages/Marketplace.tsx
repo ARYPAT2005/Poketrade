d21436b71adcfd8c71361fb7f5202aaac4ffc8c7
JoshuaJ <jkjoseph0629@gmail.com>
2025-03-27 22:21:17 -0400

    Checking out to switch to main.

--- frontend/src/pages/Marketplace.tsx
+++ frontend/src/pages/Marketplace.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import pokemonImage from "../assets/pokemon.png";
 import { isLoggedAtom } from "../atoms/isLoggedAtom";
 import { useAtomValue } from "jotai";
 
@@ -9,7 +10,26 @@ const Marketplace = () => {
   return (
     <div>
       <h1>Marketplace</h1>
-      {isLogged ? <p>Buy and sell items here!</p> : <LoginPrompt />}
+        {isLogged ? (
+          <>
+            <p style={{textAlign: "center", color: "#DADADA"}}>Buy and sell items here!</p> 
+            <div className = "container">
+              <div className = "marketplace-container">
+                {/* insert for loop here when trades have ids*/}
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={pokemonImage} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Trade</h5>
+                    <p className="card-text">Trade for this pokemon.</p>
+                    <a href="#" className="btn btn-primary">Trade</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+          </> 
+        ) : <LoginPrompt />}
     </div>
   );
 };
