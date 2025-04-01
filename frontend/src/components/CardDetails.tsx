147b9aa7404ac34dfe982e45e4d0feb57a0e7656
Jerry Wang <05jwang@gmail.com>
2025-03-31 23:59:15 -0400

    feat: Added overlay and card detail view

--- frontend/src/components/CardDetails.tsx
+++ frontend/src/components/CardDetails.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-
-import { Card as BootstrapCard } from "react-bootstrap";
-
 import Card from "../types/Card";
 
-interface CardDetailsProps {
+interface CardDetailProps {
   card: Card;
+  onClose: () => void;
 }
 
-const CardDetails: React.FC<CardDetailsProps> = ({ card }) => {
+const CardDetail: React.FC<CardDetailProps> = ({ card, onClose }) => {
   return (
-    <BootstrapCard>
-      <BootstrapCard.Body>
-        <BootstrapCard.Title>{card.name}</BootstrapCard.Title>
-        <BootstrapCard.Text>This is a card</BootstrapCard.Text>
-      </BootstrapCard.Body>
-    </BootstrapCard>
+    <div className="overlay">
+      <div className="card-detail">
+        <button onClick={onClose}>Close</button>
+        <h2>{card.name}</h2>
+        <img src={card.image_url} alt={card.name} width="300" />
+        <p>Supertype: {card.supertype}</p>
+        <p>HP: {card.hp}</p>
+        <p>Rarity: {card.rarity}</p>
+        {/* Add more card details as needed */}
+      </div>
+    </div>
   );
 };
 
-export default CardDetails;
+export default CardDetail;
