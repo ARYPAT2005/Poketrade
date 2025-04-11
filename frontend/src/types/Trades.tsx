d30badb06e9cc909ec3a25d3dce76dbefbb7ab3f
JoshuaJ <jkjoseph0629@gmail.com>
2025-04-11 00:31:56 -0400

    Added trade functionality without tranfer of cards

---
+++ frontend/src/types/Trades.tsx
@@ -0,0 +1,22 @@
+import Card from './Card';
+  
+  interface TradeCardDetail {
+    id: number;
+    card_info: Card;
+    quantity: number;
+    direction: 'offer' | 'request';
+  }
+
+  interface Trades {
+    id: number;
+    sender_username: string;
+    recipient_username: string;
+    message: string;
+    timestamp: string;
+    is_read: boolean;
+    status: 'pending' | 'accepted' | 'rejected' | 'cancelled';
+    card_details: TradeCardDetail[];
+  }
+  
+  export default Trades;
+  export type { TradeCardDetail };
